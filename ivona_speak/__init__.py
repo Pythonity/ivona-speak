2ea64f80c547bab3fd6eea9a120b982f9c69e746
Paweł Adamczak <pawel.ad@gmail.com>
2017-06-15 22:26:17 +0100

    Version bump

--- ivona_speak/__init__.py
+++ ivona_speak/__init__.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import, unicode_literals
 
 
 __title__ = 'ivona_speak'
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __author__ = 'Pythonity'
 __license__ = 'MIT'
 __url__ = 'https://github.com/Pythonity/ivona-speak'
